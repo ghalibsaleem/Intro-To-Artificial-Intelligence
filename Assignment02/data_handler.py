8a24bbbe1a17566212dc1f1cc082d24efc21f12a
Ghalib Saleem <ghalib.saleem.123@gmail.com>
2020-02-09 19:34:27 -0500

    Project created for Assignment 2

---
+++ Assignment02/data_handler.py
@@ -0,0 +1,68 @@
+from collections import deque
+
+__obj_data__ = None
+
+
+class DataClass:
+    def __init__(self):
+        self.node_list = []
+        self.node_name_list = []
+        self.graph_stack = deque()
+        self.graph = \
+            [[0 for i in range(len(self.node_list))]
+             for j in range(len(self.node_list))]
+
+
+def get_obj_data():
+    global __obj_data__
+    if __obj_data__ is None:
+        return DataClass()
+    return __obj_data__
+
+
+def init_data():
+    global __obj_data__
+    __obj_data__ = DataClass()
+
+
+class Nodes:
+    def __init__(self, name, x, y):
+        self.x = x
+        self.y = y
+        self.name = name
+        self.is_blocked = False
+
+
+def read_from_file(start_node, end_node, locations_file, connections_file):
+    try:
+        global __obj_data__
+        node_file = open(locations_file)
+        for line in node_file:
+            node_data = line.split()
+            if len(node_data) > 1:
+                __obj_data__.node_list.append(
+                    Nodes(node_data[0], int(node_data[1]), int(node_data[2]))
+                )
+        node_file.close()
+        __obj_data__.node_list.sort(key=lambda each_node: each_node.name)
+        __obj_data__.node_name_list = \
+            [temp_node.name for temp_node in __obj_data__.node_list]
+        if __obj_data__.node_name_list.index(start_node) < 0:
+            raise Exception("There is no start node in the graph")
+        if __obj_data__.node_name_list.index(end_node) < 0:
+            raise Exception("There is no end node in the graph")
+        __obj_data__.graph = [[0 for i in range(len(__obj_data__.node_list))]
+                              for j in range(len(__obj_data__.node_list))]
+        node_file = open(connections_file)
+        for line in node_file:
+            node_data = line.split()
+            if len(node_data) > 1:
+                columns = __obj_data__.node_name_list.index(node_data[0])
+                count = int(node_data[1])
+                for x in range(0, count):
+                    row = __obj_data__.node_name_list.index(node_data[2 + x])
+                    __obj_data__.graph[columns][row] = 1
+        node_file.close()
+        return 1
+    except Exception as e:
+        raise Exception("Something went wrong. " + str(e))
---
+++ Assignment02/main.py
@@ -0,0 +1,37 @@
+import traceback
+import sys
+from data_handler import init_data, read_from_file
+
+
+def main(arguments):
+    try:
+        init_data()
+        if len(arguments) < 3:
+            raise Exception("Locations and Connections file paths are " +
+                            "required as input")
+        locations_file = arguments[1]
+        connections_file = arguments[2]
+        start_node = input("Enter the start point of the graph: ")
+        end_node = input("Enter the end point of the graph: ")
+        number_of_paths = input("Enter number of path you want to see: ")
+        if not number_of_paths:
+            number_of_paths = 1
+        else:
+            number_of_paths = int(number_of_paths)
+        ret_value = read_from_file(start_node, end_node, locations_file,
+                                  connections_file)
+        if ret_value < 0:
+            raise Exception("Something blew up while reading the input files.")
+        # shortest_path = get_shortest_path(start_node, end_node, number_of_paths)
+        # print("The Shortest Path among the displayed paths from " +
+        #       start_node + " ------> " + end_node + " : " +
+        #       str(shortest_path[2]))
+        # print("The overall Shortest Path from " + start_node + " ------> " +
+        #       end_node + " : " + str(shortest_path[1]))
+    except Exception as e:
+        traceback.print_exc()
+        raise Exception("Something went wrong. " + str(e))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
